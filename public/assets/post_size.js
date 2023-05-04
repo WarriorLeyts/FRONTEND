99f05bc9aac3ad296fe65fc638dc1fef26c25b33
Magomed Rasaev <daye.leyts95@gmail.com>
2023-05-04 03:54:29 +0300

    Функция была доработана

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,16 +1,16 @@
 export default function posSize(message) {
-    let arrDomen = ['www','http','https','.biz','.com','.edu','.gov','.info','.mil','.movie',
-                   '.museum','.net','.org','.pro','.tv','.name','.club','.xyz'];
-    let arrMessage = [];
-    let url = ''; 
+  let arrDomen = ['.biz','.com','.edu','.gov','.info','.mil','.movie',
+                 '.su','.net','.org','.pro','.tv','.name','.ru','.xyz'];
+  let arrMessage = [];
 
-    arrMessage = message.split(' ') // разбиваем строку на массив 
-    
-    arrDomen.forEach( domen =>{  // находим элемент массива с доменом
-       arrMessage.forEach(el => {
-         if (el.includes(domen)) url = el        // присваиваем найденный элемент к переменной
-       });
-     })
-     
-    return message.replace(url,'').length  // возвращаем длину строки, после удаления "url"
-}   
+  arrMessage = message.split(' ') // разбиваем строку на массив 
+  
+  arrDomen.forEach( domen =>{  // находим элемент массива с доменом
+     arrMessage.forEach(el => {
+       if (el.includes(domen)) {
+        message = message.replace(el,'') }       // удаляем url
+     });
+   })
+   
+  return message.length // возвращаем длину строки
+}
