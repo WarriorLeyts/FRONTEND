3520d423eedc26f26f9fdf3ac2af6b9487aa539c
Magomed Rasaev <daye.leyts95@gmail.com>
2023-05-09 16:45:35 +0300

    Настройка тестов и линтера

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,16 +1,16 @@
-export default function posSize(message) {
-  let arrDomen = ['.biz','.com','.edu','.gov','.info','.mil','.movie',
-                 '.su','.net','.org','.pro','.tv','.name','.ru','.xyz'];
+export default function postSize(message) {
+  const arrDomen = ['.biz', '.com', '.edu', '.gov', '.info', '.mil', '.movie', '.su', '.net', '.org', '.pro', '.tv', '.name', '.ru', '.xyz'];
   let arrMessage = [];
+  let res = message;
+  arrMessage = message.split(' '); // разбиваем строку на массив
 
-  arrMessage = message.split(' ') // разбиваем строку на массив 
-  
-  arrDomen.forEach( domen =>{  // находим элемент массива с доменом
-     arrMessage.forEach(el => {
-       if (el.includes(domen)) {
-        message = message.replace(el,'') }       // удаляем url
-     });
-   })
-   
-  return message.length // возвращаем длину строки
+  arrDomen.forEach((domen) => { // находим элемент массива с доменом
+    arrMessage.forEach((el) => {
+      if (el.includes(domen)) {
+        res = message.replace(el, ''); // удаляем url
+      }
+    });
+  });
+
+  return res.length; // возвращаем длину строки
 }
