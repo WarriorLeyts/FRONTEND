9eaf7e7430ce25c2fa39963df4136d3f2dc0f1ae
WarriorLeyts <daye.leyts95@gmail.com>
2023-07-28 04:20:04 +0300

    Добавил функцию открытия и закрытия модальных окон

--- public/assets/index.js
+++ public/assets/index.js
@@ -29,20 +29,20 @@ EX1.addEventListener('click', () => {
 EX2.addEventListener('click', () => {
   REGAUTHOR.style.display = 'none';
 });
-BLREGWINDOW.addEventListener('touchmove', () => {
+HANDEX1.addEventListener('touchmove', () => {
   HANDEX1.style.opacity = 0;
   BLREGWINDOW.style.transform = 'translate(0px, 800px)';
 });
-BLREGWINDOW.addEventListener('touchend', () => {
+HANDEX1.addEventListener('touchend', () => {
   BLREGWINDOW.style.transform = 'none';
   HANDEX1.style.opacity = 1;
   REGWINDOW.style.display = 'none';
 });
-BLAUTHORWINDOW.addEventListener('touchmove', () => {
+HANDEX2.addEventListener('touchmove', () => {
   HANDEX2.style.opacity = 0;
   BLAUTHORWINDOW.style.transform = 'translate(0px, 800px)';
 });
-BLAUTHORWINDOW.addEventListener('touchend', () => {
+HANDEX2.addEventListener('touchend', () => {
   BLAUTHORWINDOW.style.transform = 'none';
   HANDEX2.style.opacity = 1;
   REGAUTHOR.style.display = 'none';
