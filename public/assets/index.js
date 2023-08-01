f9e0dfd236c6c89062354466f9095a94ebc0b4d8
WarriorLeyts <daye.leyts95@gmail.com>
2023-08-01 22:54:49 +0300

    Сделал валидацию для формы регистрации

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,3 +1,5 @@
+import emailValidation from './email_validation.js';
+
 const EX1 = document.getElementById('ex1');
 const EX2 = document.getElementById('ex2');
 const HANDEX1 = document.getElementById('handler-ex1');
@@ -10,6 +12,11 @@ const REGWINDOW = document.getElementById('registration-window');
 const BLREGWINDOW = document.getElementById('block-registration-window');
 const BLAUTHORWINDOW = document.getElementById('block-authorization-window');
 const REGAUTHOR = document.getElementById('authorization-window');
+const BTNREGWIN = document.getElementById('btn-registration-window');
+const INPREGLOGIN = document.getElementById('input-reg-login');
+const INPREGMAIL = document.getElementById('input-reg-mail');
+const INPREGPASS = document.getElementById('input-reg-pass');
+const INPREGPASSCONF = document.getElementById('input-reg-pass-conf');
 BTNREGISTR1.addEventListener('click', () => {
   REGWINDOW.style.display = 'flex';
 });
@@ -46,4 +53,77 @@ HANDEX2.addEventListener('touchend', () => {
   BLAUTHORWINDOW.style.transform = 'none';
   HANDEX2.style.opacity = 1;
   REGAUTHOR.style.display = 'none';
+});
+BTNREGWIN.addEventListener('click', () => {
+  if (INPREGLOGIN.querySelector('.window-input__input').value === '') {
+    INPREGLOGIN.querySelector('.login-vd').innerHTML = 'Укажите имя';
+    INPREGLOGIN.querySelector('.login-vd').style.display = 'block';
+    INPREGLOGIN.querySelector('.window-input').style.marginBottom = '0px';
+    INPREGLOGIN.querySelector('.window-input').style.border = '1px solid #FF97C3';
+    INPREGLOGIN.querySelector('.window-input').style.background = '#FFDEEC';
+    INPREGLOGIN.querySelector('.window-input__input').style.background = '#FFDEEC';
+  } else {
+    INPREGLOGIN.querySelector('.login-vd').style.display = 'none';
+    INPREGLOGIN.querySelector('.window-input').style.marginBottom = '24px';
+    INPREGLOGIN.querySelector('.window-input').style.border = '1px solid #DFDFDF';
+    INPREGLOGIN.querySelector('.window-input').style.background = '#FFF';
+    INPREGLOGIN.querySelector('.window-input__input').style.background = '#FFF';
+  }
+  if (INPREGMAIL.querySelector('.window-input__input').value === '') {
+    INPREGMAIL.querySelector('.mail-vd').innerHTML = 'Укажите адрес почты';
+    INPREGMAIL.querySelector('.mail-vd').style.display = 'block';
+    INPREGMAIL.querySelector('.window-input').style.marginBottom = '0px';
+    INPREGMAIL.querySelector('.window-input').style.border = '1px solid #FF97C3';
+    INPREGMAIL.querySelector('.window-input').style.background = '#FFDEEC';
+    INPREGMAIL.querySelector('.window-input__input').style.background = '#FFDEEC';
+  } else if (!emailValidation(INPREGMAIL.querySelector('.window-input__input').value)) {
+    INPREGMAIL.querySelector('.mail-vd').innerHTML = 'Адрес не валиден';
+    INPREGMAIL.querySelector('.mail-vd').style.display = 'block';
+    INPREGMAIL.querySelector('.window-input').style.marginBottom = '0px';
+    INPREGMAIL.querySelector('.window-input').style.border = '1px solid #FF97C3';
+    INPREGMAIL.querySelector('.window-input').style.background = '#FFDEEC';
+    INPREGMAIL.querySelector('.window-input__input').style.background = '#FFDEEC';
+  } else {
+    INPREGMAIL.querySelector('.mail-vd').style.display = 'none';
+    INPREGMAIL.querySelector('.window-input').style.marginBottom = '24px';
+    INPREGMAIL.querySelector('.window-input').style.border = '1px solid #DFDFDF';
+    INPREGMAIL.querySelector('.window-input').style.background = '#FFF';
+    INPREGMAIL.querySelector('.window-input__input').style.background = '#FFF';
+  }
+  if (INPREGPASS.querySelector('.window-input__input').value === '') {
+    INPREGPASS.querySelector('.password-vd').innerHTML = 'Укажите пароль';
+    INPREGPASS.querySelector('.password-vd').style.display = 'block';
+    INPREGPASS.querySelector('.window-input').style.marginBottom = '0px';
+    INPREGPASS.querySelector('.window-input').style.border = '1px solid #FF97C3';
+    INPREGPASS.querySelector('.window-input').style.background = '#FFDEEC';
+    INPREGPASS.querySelector('.window-input__input').style.background = '#FFDEEC';
+  } else {
+    INPREGPASS.querySelector('.password-vd').style.display = 'none';
+    INPREGPASS.querySelector('.window-input').style.marginBottom = '24px';
+    INPREGPASS.querySelector('.window-input').style.border = '1px solid #DFDFDF';
+    INPREGPASS.querySelector('.window-input').style.background = '#FFF';
+    INPREGPASS.querySelector('.window-input__input').style.background = '#FFF';
+  }
+  if (INPREGPASS.querySelector('.window-input__input').value !== INPREGPASSCONF.querySelector('.window-input__input').value) {
+    INPREGPASSCONF.querySelector('.confirm-password-vd').innerHTML = 'пароли не совпадают';
+    INPREGPASSCONF.querySelector('.confirm-password-vd').style.display = 'block';
+    INPREGPASSCONF.querySelector('.window-input').style.marginBottom = '0px';
+    INPREGPASSCONF.querySelector('.window-input').style.border = '1px solid #FF97C3';
+    INPREGPASSCONF.querySelector('.window-input').style.background = '#FFDEEC';
+    INPREGPASSCONF.querySelector('.window-input__input').style.background = '#FFDEEC';
+  } else {
+    INPREGPASSCONF.querySelector('.confirm-password-vd').style.display = 'none';
+    INPREGPASSCONF.querySelector('.window-input').style.marginBottom = '24px';
+    INPREGPASSCONF.querySelector('.window-input').style.border = '1px solid #DFDFDF';
+    INPREGPASSCONF.querySelector('.window-input').style.background = '#FFF';
+    INPREGPASSCONF.querySelector('.window-input__input').style.background = '#FFF';
+  }
+  if (INPREGLOGIN.querySelector('.login-vd').style.display === 'none'
+  && INPREGMAIL.querySelector('.mail-vd').style.display === 'none'
+  && INPREGPASS.querySelector('.password-vd').style.display === 'none'
+  && INPREGPASSCONF.querySelector('.confirm-password-vd').style.display === 'none') {
+    console.log(INPREGLOGIN.querySelector('.window-input__input').value);
+    console.log(INPREGMAIL.querySelector('.window-input__input').value);
+    console.log(INPREGPASS.querySelector('.window-input__input').value);
+  }
 });
