13544e7d87da92acc1b6ddbfbb9b80e4adda3da1
WarriorLeyts <daye.leyts95@gmail.com>
2023-10-27 16:32:33 +0300

    Перенес приложение в Express

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import fs from 'fs';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const html = fs.readFileSync('public/main.html', 'utf8');
 
@@ -11,5 +11,5 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.type('html').send(html));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
