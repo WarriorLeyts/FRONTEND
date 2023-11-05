99602fb350a431394fef335b7652aa0cf650a1b2
WarriorLeyts <daye.leyts95@gmail.com>
2023-11-05 23:59:47 +0300

    Подключил PostgreSQL в express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import fs from 'fs';
+import pkg from 'pg';
 
 const app = express();
 const port = 3000;
@@ -13,3 +14,22 @@ app.get('/', (req, res) => res.type('html').send(html));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const { Client } = pkg;
+const client = new Client({
+  user: 'muhammad',
+  host: 'dpg-cl0dlc237rbc739gj0q0-a.oregon-postgres.render.com',
+  database: 'demos',
+  password: 'XTF7IRWVdMfChArk3mQpUEFvEcE9WZ1N',
+  port: 5432,
+  ssl: true,
+});
+const query1 = `SELECT * 
+  FROM Posts, Users
+  WHERE Posts.user_id=Users.user_id`;
+client.connect();
+const posts = (await client.query(query1)).rows;
+
+app.get('/posts.json', (req, res) => res.send(posts));
+
+client.end();
