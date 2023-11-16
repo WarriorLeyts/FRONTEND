910a6ee1d1013ffb6e2060b65d69d27515752fb6
WarriorLeyts <daye.leyts95@gmail.com>
2023-11-17 00:44:07 +0300

    Добавил еще 3 метода API: Создать, удалить и редактировать пост

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = 3000;
 const html = fs.readFileSync('public/main.html', 'utf8');
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.type('html').send(html));
 
@@ -24,12 +26,28 @@ const client = new Client({
   port: 5432,
   ssl: true,
 });
-const query1 = `SELECT * 
+const queryGetPosts = `SELECT * 
   FROM Posts, Users
   WHERE Posts.user_id=Users.user_id`;
 client.connect();
-const posts = (await client.query(query1)).rows;
-
+const posts = JSON.stringify((await client.query(queryGetPosts)).rows);
 app.get('/posts.json', (req, res) => res.send(posts));
 
-client.end();
+app.post('/posts.json', (req) => {
+  const queryCreatePost = `INSERT INTO Posts (user_id, date, message, img_message) 
+  VALUES (${+req.body.user_id},'${req.body.date}','${req.body.message}','${req.body.img_message}')`;
+  client.query((queryCreatePost));
+});
+
+app.delete('/posts/1.json', (req, res) => {
+  res.send('DELETE Request Called');
+  const queryDeletePost = `DELETE FROM Posts WHERE post_id = ${req.body.post_id};`;
+  client.query((queryDeletePost));
+});
+app.post('/posts/1.json', (req) => {
+  const arrKeys = Object.keys(req.body.edit);
+  const arrValues = Object.values(req.body.edit);
+  const valuesEdit = arrKeys.map((item, index) => `${item} = '${arrValues[index]}'`).join(',');
+  const queryEditPost = `UPDATE Posts SET ${valuesEdit} WHERE post_id = ${req.body.post_id}`;
+  client.query((queryEditPost));
+});
