7435cc05d72f3f8ded038b963a1557d5bcabb581
WarriorLeyts <daye.leyts95@gmail.com>
2024-07-25 00:40:06 +0300

    Перенес web прилодение в реакт 1

--- src/components/AuthorizationFooter.jsx
+++ src/components/AuthorizationFooter.jsx
@@ -1,16 +1,17 @@
 import styles from '@/styles/AuthorizationFooter.module.css';
 import { useState } from 'react'
-//import { useFetchStore } from '@/store/fetch.js';
+import usemodalActive from '@/store/modal.js';
 
 const AuthorizationFooter = () => {
-    const [modalActive, setModalActive] = useState(true)
+    const setRegModalActive = usemodalActive ((state) => state.setRegModalActive);
+    const setAuthorModalActive = usemodalActive ((state) => state.setAuthorModalActive);
     return (
     <section className={styles.authorizationFooter}>  
         <div className={styles.authorization}>
             <h1 className={styles.authorization__title}> Зарегистрируйтесь и узнайте обо всём первым</h1>
             <div className={styles.authorization__block}>
-                <button className={styles.authorization__btn}>Зарегистрироваться</button>
-                <button className={styles.authorization__btn}>Войти</button>
+                <button className={styles.authorization__btn} onClick={() => setRegModalActive()}>Зарегистрироваться</button>
+                <button className={styles.authorization__btn} onClick={() => setAuthorModalActive()}>Войти</button>
             </div>
         </div>
     </section>
