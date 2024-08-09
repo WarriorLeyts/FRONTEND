6da1ddfbc14558c75418e0a5fd528130628e301d
WarriorLeyts <daye.leyts95@gmail.com>
2024-08-09 18:56:15 +0300

    Сделал верстку страницы /feed

---
+++ src/components/CardUser.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import styles from '../styles/cardUser.module.css';
+const CardUser = () => {
+    return (
+        <div className={styles.cardUser}>
+            <div className={styles.userInfoBlock}>
+                <img className={styles.avatar} src="img/alexsandr-avatar.png"/>
+                <div className={styles.userInfo}>
+                  <p className={styles.name}>Александр</p>
+                  <span className={styles.email}>@burtovoy</span>
+                </div>
+            </div>    
+                <div className={styles.statistics}>
+                    <div className={styles.statistic}>
+                      <p className={styles.title}>45K</p>
+                      <span className={styles.subTitle}>Сообщений</span>
+                    </div>
+                    <div className={styles.statistic}>
+                      <p className={styles.title}>28</p>
+                      <span className={styles.subTitle}>Читаемых</span>
+                    </div>
+                    <div className={styles.statistic}>
+                      <p className={styles.title}>118</p>
+                      <span className={styles.subTitle}>Читателей</span>
+                    </div>                   
+                </div>
+        </div>
+    );
+};
+export default CardUser;
