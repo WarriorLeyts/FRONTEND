871bdaf0a48d354e31bdc6d823a48f4077ec9885
WarriorLeyts <daye.leyts95@gmail.com>
2024-09-27 01:10:54 +0300

    Реализовано отображение аватара пользователя в шапке сайта

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavbarList from './NavbarList'
 import styles from '../styles/FeedPage.module.css';
+import { useSelector } from 'react-redux';
 
 export default function Footer({location}) {
-  const location1 = location
+  const location1 = location;
+  const { userInfo } = useSelector((state) => state.profile);
+  const [avatarImg, setAvatarImg] = useState('');
+  useEffect(() => {
+    setAvatarImg(userInfo?.avatar)
+  }, [userInfo])
   return (
     <footer className={styles.footer}>
-         <NavbarList location={location1}/> <img className={styles.avatar}src='../img/alexsandr-avatar.png'/>
+         <NavbarList location={location1}/> <div className={styles.avatar} style={{backgroundImage: `url(${avatarImg  || ''})`}}></div>
         </footer>
   )
 }
